a6872b9d68ada6badb00fd0b9e150489f4ac476f
charles1stgithub <charleslester.official@gmail.com>
2025-01-16 17:06:20 +0800

    Registration Authentication And Error Messages To The Input Fields

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MyTodosComponent } from './pages/my-todos/my-todos.component';
+import { AdminComponent } from './pages/admin/admin/admin.component';
+import { UserTodosComponent } from './pages/admin/user-todos/user-todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'my-todos',
+        component: MyTodosComponent,
+      },
+      {
+        path: 'admin',
+        component: AdminComponent,
+      },
+      {
+        path: 'admin/user-todos',
+        component: UserTodosComponent,
+      },
+    ],
+  },
+];
