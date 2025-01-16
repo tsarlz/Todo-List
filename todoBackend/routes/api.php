e35dca90ae283b7226f4e44d5522b7f7f253471b
charles1stgithub <charleslester.official@gmail.com>
2025-01-16 09:18:07 +0800

    Finish Backend CRUD and Authentication Features

--- todoBackend/routes/api.php
+++ todoBackend/routes/api.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+
 
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
